09e00c2fcbf859ca7c5f0680b64b9d3d446c92a0
SiddharthCA96 <ss6156852@gmail.com>
2025-01-16 09:10:31 +0530

    calculated the tf_score dict

--- scrapping/tf-idf.py
+++ scrapping/tf-idf.py
@@ -17,7 +17,10 @@ stop_words = set(stopwords.words('english'))
 prob_des=[]
 all_key_words=[]
 total_doc=1737
+# dict to store thr cnt of how many docs contain a given key_word
+nt={}
 
+# get all the key_words from all the  docs
 for i in range(1,total_doc+1):
   with open("Corpus/leet_prob"+str(i)+".txt") as f:
     doc=f.read()
@@ -34,4 +37,31 @@ for i in range(1,total_doc+1):
 (all_key_words).sort()
 # print(len(all_key_words)) 
         
-    
+# calculate the tf score vector
+for i in range(1,total_doc+1):
+  total_words=prob_des[i-1].split()
+  total_kword_doc=0
+  tf_score={}
+  key_words=[]
+  for each_word in total_words:
+    each_word=each_word.replace('.','')
+    if each_word not in stop_words:
+      total_kword_doc+=1
+      if each_word not in key_words:
+        key_words.append(each_word)
+      if each_word in tf_score:
+        tf_score[each_word]+=1
+      else:
+        tf_score[each_word]=1
+  # fill the nt dict
+  for ele in all_key_words:
+    if ele in key_words:
+      if ele in nt:
+        nt[ele]+=1
+      else:
+        nt[ele]=1
+    else:
+      tf_score[ele]=0
+  # update the tf score
+  tf_score.update((x,y/int(total_kword_doc)) for x,y in tf_score.items())
+  # print(tf_score)
