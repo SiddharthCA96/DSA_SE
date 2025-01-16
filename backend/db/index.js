c74a33b6c18e2be58b4f0339475e9d6dce67f3b0
SiddharthCA96 <ss6156852@gmail.com>
2025-01-16 18:28:31 +0530

    created mongodb models

--- backend/db/index.js
+++ backend/db/index.js
@@ -38,7 +38,39 @@ const UserSchema=new mongoose.Schema({
     },
 });
 
-//create the user model
+//keyword schema
+const all_keywordsSchema=new mongoose.Schema({
+    keyword_values:String,
+})
+
+//idf schema
+const idfSchema=new mongoose.Schema({
+    idf_values:String,
+})
+
+//tf-idf schema
+const tf_idfSchema=new mongoose.Schema({
+    tf_idf_values:String,
+})
+//magnitude schema
+const magnitudeSchema=new mongoose.Schema({
+    mag_values:String,
+})
+//problem schema
+const problemSchema=mongoose.Schema({
+    problem_desc:String,
+    problem_url:String,
+    problem_title:String,
+    problem_id:Number,
+    problem_mag:Number
+})
+//create the  models
+
+//user model
 export const User=mongoose.model('User',UserSchema);
-
-
+//keyword model
+export const Db_Keyword=mongoose.model('Db_Keyword',all_keywordsSchema);
+export const idf=mongoose.model('idf',idfSchema);
+export const tf_idf=mongoose.model('tf_idf',tf_idfSchema);
+export const Db_mag=mongoose.model('Db_mag',magnitudeSchema);
+export const all_problem=mongoose.model('all_problem',all_problem);
